80c2b31a33b503125d6f991771263b3b9f6f8c53
MinYongUm <minyong_e89@naver.com>
2026-02-04 16:47:41 +0900

    feat: command_list.py 수정

--- config/command_list.py
+++ config/command_list.py
@@ -4,37 +4,77 @@
 # MGMT 장비 명령어 (IOS-XE)
 # =================================================================
 MGMT_COMMANDS_TO_RUN = [
-    'show running-config',
-    'show startup-config',
+    'terminal length 0',
     'show version',
-    'show inventory',
-    'show interfaces status',
-    'show vlan brief',
-    'show etherchannel summary',
+    'show int status',
+    'show int count errors',
+    'show ip int brief',
     'show ip route',
+    'show int trunk',
+    'show int transceiver detail',
+    'show spanning-tree su',
+    'show spanning-tree root',
+    'show spanning-tree',
+    'show vlan brief',
+    'show module',
+    'show ntp status',
+    'show boot',
+    'show running-config',
+    'show log',
+    'terminal length 49'
 ]
 
 # =================================================================
 # NEXUS 장비 명령어 (NX-OS)
 # =================================================================
 NEXUS_COMMANDS_TO_RUN = [
-    'show running-config',
-    'show startup-config',
+    'terminal length 0',
     'show version',
-    'show inventory',
-    'show interface status',
+    'show int status',
+    'show int count errors',
+    'show ip int brief vrf all',
+    'show ip route vrf all',
+    'show int trunk',
+    'show int transceiver detail',
+    'show port-channel su',
+    'show spanning-tree su',
+    'show spanning-tree root',
+    'show spanning-tree',
     'show vlan brief',
-    'show vpc',
-    'show port-channel summary',
+    'show hsrp brief',
+    'show system resources',
+    'show env',
+    'show core',
+    'show module',
+    'show ntp peers',
+    'show ntp status',
+    'show boot',
+    'show running-config',
+    'show logging log',
+    'terminal length 49'
 ]
+
 
 # =================================================================
 # ACI 장비 명령어 (Spine/Leaf)
 # =================================================================
 ACI_COMMANDS_TO_RUN = [
-    'show running-config',
+    'show int status',
     'show version',
-    'show inventory',
-    'show interface status',
+    'show int count errors',
+    'show ip int brief vrf all',
+    'show ip route vrf all',
+    'show int trunk',
+    'show int transceiver detail',
+    'show port-channel su',
+    'show mcp internal info vlan all',
+    'show vlan brief',
+    'show hsrp brief',
+    'show system resources',
+    'show env',
+    'show core',
+    'show module',
+    'show ntp peers',
     'show lldp neighbors',
+    'show cdp neighbors '
 ]
